f7abd6d41f3ff949c1105d83054fca65ee7b3ed7
el-the-mundo <130784209+el-the-mundo@users.noreply.github.com>
2025-01-12 22:13:18 +0000

    N

--- script.lua
+++ script.lua
@@ -3,6 +3,28 @@ local ImGui = loadstring(game:HttpGet(SourceURL))()
 
 local Window = ImGui:CreateWindow({
 	Title = "Window",
-	Size = UDim2.fromOffset(250, 300),
+	Size = UDim2.fromOffset(400, 250),
 	Position = UDim2.new(0.5, 0, 0, 70),
 })
+
+local ActiveTabs = {
+    Main = Window:CreateTab({
+        Name = "ROGUE LINEAGE";
+        Visible = true;
+    });
+
+    Player = Window:CreateTab({
+        Name = "PLAYER";
+        Visible = true;
+    });
+
+    Settings = Window:CreateTab({
+        Name = "SETTINGS";
+        Visible = true;
+    });
+
+    Debug = Window:CreateTab({
+        Name = "DEBUG";
+        Visible = true;
+    });
+}
