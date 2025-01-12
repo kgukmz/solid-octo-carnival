1552f07d6837a8371ac4c1b07f703c1937c27389
el-the-mundo <130784209+el-the-mundo@users.noreply.github.com>
2025-01-12 22:07:48 +0000

    V

--- script.lua
+++ script.lua
@@ -1,1 +1,8 @@
+local SourceURL = 'https://github.com/depthso/Roblox-ImGUI/raw/main/ImGui.lua'
+local ImGui = loadstring(game:HttpGet(SourceURL))()
 
+local Window = ImGui:CreateWindow({
+	Title = "Window",
+	Size = UDim2.fromOffset(250, 300),
+	Position = UDim2.new(0.5, 0, 0, 70),
+})
