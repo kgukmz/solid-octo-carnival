8ebfe192165cc49fea1a58ba0d26294549990e6e
el-the-mundo <130784209+el-the-mundo@users.noreply.github.com>
2025-01-13 21:00:16 +0000

    a lot

--- script.lua
+++ script.lua
@@ -1,4 +1,5 @@
 local Players = cloneref(game:GetService("Players"))
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RunService = cloneref(game:GetService("RunService"))
 local CollectionService = cloneref(game:GetService("CollectionService"))
 local Lighting = cloneref(game:GetService("Lighting"))
@@ -17,15 +18,26 @@ local IdentifyExecutor = identifyexecutor()
 local PlayerTable = {}
 
 getgenv().ScriptData = {
+    Main = {
+        InstantMenu = false;
+
+    };
+
     Player = {
         InfiniteJump = false;
+        NoFallDamage = false;
         KillMethod = nil;
+
+        SpeedhackEnabled = false;
+        SpeedhackSpeed = 10;
+
+        PulseType = "LifeSense";
     };
     SelectedCharacterView = nil;
 }
 
 local Window = ImGui:CreateWindow({
-	Title = "Window" .. " | " .. IdentifyExecutor,
+	Title = "Alchemy" .. " | " .. IdentifyExecutor,
 	Size = UDim2.fromOffset(400, 250),
 	Position = UDim2.new(0.5, 0, 0, 70),
     NoResize = true,
@@ -110,34 +122,41 @@ end)
 
 -- // Rogue Lineage 
 
+local function FireMenu()
+    local ReturnToMenu = ReplicatedStorage.Requests.ReturnToMenu
+    DebugConsoleLog(ReturnToMenu:InvokeServer())
+end
+
 local TabMain = ActiveTabs.Main
 
 TabMain:Separator({
     Text = "ROGUE LINEAGE";
 })
 
-local CharacterViewHeader = TabMain:CollapsingHeader({
-	Title = "Character View"
-})
+do -- // Automation
+    local AutomationHeader = TabMain:CollapsingHeader({
+        Title = "Automation"
+    })
 
-local CharacterViewport = CharacterViewHeader:Viewport({
-	Size = UDim2.fromOffset(350, 150),
-    Clone = true,
-    Model = nil,
-    Border = true,
-})
+    AutomationHeader:Checkbox({
+        Label = "Instant Menu";
+        Callback = function(self, Value)
+            getgenv().ScriptData.Main.InstantMenu = Value
+        end
+    })
 
-local Rig = nil
+    AutomationHeader:Keybind({
+        Label = "Instant Menu Keybind",
+        IgnoreGameProcessed = false;
+        Callback = function()
+            if (getgenv().ScriptData.Main.InstantMenu == false) then
+                return
+            end
 
-CharacterViewHeader:Combo({
-    Placeholder = "...";
-    Label = "Players";
-    Items = PlayerTable;
-    Callback = function(self, Value)
-       Rig = CharacterViewport:SetModel(Value, CFrame.new(0, -2.5, -5))
-    end
-})
-
+            FireMenu()
+        end,
+    })
+end
 
 -- // Player
 
@@ -150,6 +169,29 @@ local function Reset()
     end
 
     Head:Destroy()
+end
+
+local function EnablePulse(Type, Value)
+    if (Value == true) then
+        local PulseType = Instance.new("Accessory")
+        PulseType.Name = Type
+        PulseType.Parent = Player.Character
+
+        CollectionService:AddTag(PulseType, "1e9")
+    else
+        for _, PulseType in next, {"LifeSense", "WorldPulseUltra", "WorldPulse"} do
+            local PulseInstance = Player.Character:FindFirstChild(PulseType)
+            if (not PulseInstance) then
+                continue
+            end
+
+            if (not CollectionService:HasTag(PulseInstance, "1e9")) then
+                continue
+            end
+
+            PulseInstance:Destroy()
+        end
+    end
 end
 
 local function Kill()
@@ -167,8 +209,12 @@ local function Kill()
 
         local SolansSword
 
-        for _,Obj in next, workspace:GetDescendants() do
+        for _,Obj in next, getinstances do
             if (Obj.Name ~= "SealedSword") then
+                continue
+            end
+
+            if (Obj.Parent.Name ~= "Folder") then
                 continue
             end
 
@@ -186,43 +232,107 @@ local function Kill()
 end
 
 local TabPlayer = ActiveTabs.Player
-local PlayerRow = ActiveTabs.Player:Row()
 
-PlayerRow:Button({
-    Text = "Reset";
-    Callback = Reset
-})
+do  -- // Button row
+    local PlayerRow = ActiveTabs.Player:Row()
 
-PlayerRow:Button({
-    Text = "Kill Self";
-    Callback = Kill
-})
+    PlayerRow:Button({
+        Text = "Reset";
+        Callback = Reset
+    })
 
-TabPlayer:Separator({
-    Text = "PLAYER";
-})
+    PlayerRow:Button({
+        Text = "Kill Self";
+        Callback = Kill
+    })
 
-TabPlayer:Combo({
-    Placeholder = "...";
-    Label = "Kill Method";
-    Items = {
-        "Solans"
-    };
-    Callback = function(self, Value)
-        getgenv().ScriptData.Player.KillMethod = Value
-    end;
-})
+    PlayerRow:Fill()
+end
 
-TabPlayer:Checkbox({
-    Label = "No Injuries";
-})
+do -- // Player Features General
+    TabPlayer:Separator({
+        Text = "PLAYER";
+    })
+    
+    TabPlayer:Combo({
+        Placeholder = "...";
+        Label = "Kill Method";
+        Items = {
+            "Solans"
+        };
+        Callback = function(self, Value)
+            getgenv().ScriptData.Player.KillMethod = Value
+        end;
+    })
+    
+    TabPlayer:Checkbox({
+        Label = "No Injuries";
+    })
+    
+    TabPlayer:Checkbox({
+        Label = "No Fall Damage";
+        Callback = function(self, Value)
+            getgenv().ScriptData.Player.NoFallDamage = Value
+        end
+    })
+    
+    TabPlayer:Checkbox({
+        Label = "Infinite Jump";
+        Callback = function(self, Value)
+            getgenv().ScriptData.Player.InfiniteJump = Value
+        end
+    })
+end
 
-TabPlayer:Checkbox({
-    Label = "Infinite Jump";
-    Callback = function(self, Value)
-        getgenv().ScriptData.Player.InfiniteJump = Value
-    end
-})
+do -- // Speedhack
+    local SpeedhackHeader = TabPlayer:CollapsingHeader({
+        Title = "Speedhack";
+    })
+    
+    SpeedhackHeader:Slider({
+        Label = "Speed";
+        Format = "%.d/%s";
+        Value = 10;
+        MinValue = 10;
+        MaxValue = 250;
+        Callback = function(self, Value)
+            getgenv().ScriptData.Player.SpeedhackSpeed = Value
+        end,
+    })
+    
+    SpeedhackHeader:Checkbox({
+        Label = "Enable Speedhack";
+        Callback = function(self, Value)
+            getgenv().ScriptData.Player.SpeedhackEnabled = Value
+        end
+    })
+end
+
+do -- // Pulses
+    local PulseHeader = TabPlayer:CollapsingHeader({
+	    Title = "Life Sense / World's Pulse";
+    })
+
+    PulseHeader:Combo({
+        Selected = getgenv().ScriptData.Player.PulseType;
+        Label = "Pulse Type";
+        Items = {
+            "LifeSense";
+            "WorldPulseUltra";
+            "WorldPulse";
+        };
+        Callback = function(self, Value)
+            getgenv().ScriptData.Player.PulseType = Value
+        end;
+    })
+
+    PulseHeader:Checkbox({
+        Label = "Enable Pulse";
+        Callback = function(self, Value)
+            EnablePulse(getgenv().ScriptData.Player.PulseType, Value)
+        end
+    })
+end
 
 -- // Debug
 
@@ -242,6 +352,25 @@ getgenv().DebugConsole = TabDebug:Console({
 	AutoScroll = true,
 	MaxLines = math.huge
 })
+
+-- // Settings
+
+local TabSettings = ActiveTabs.Settings
+
+do -- // General
+    TabSettings:Separator({
+        Text = "SETTINGS";
+    })
+
+    TabSettings:Keybind({
+        Label = "Toggle UI",
+        Value = Enum.KeyCode.Insert,
+        Callback = function()
+            Window:SetVisible(not Window.Visible)
+        end,
+    })
+end
+
 
 local function ClientAntiBan()
     for i, v : AnimationTrack in next, Humanoid:GetPlayingAnimationTracks() do
@@ -264,11 +393,18 @@ local function RunServiceChecks()
 
         Player.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
     end
+
+    if (Player.Character.Humanoid:GetState() == Enum.HumanoidStateType.Freefall) then
+        if (getgenv().ScriptData.Player.NoFallDamage == false) then
+            return
+        end
+
+        Player.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Running)
+    end
 end
 
 RunService:BindToRenderStep("AntiBan", 1, ClientAntiBan)
 RunService:BindToRenderStep("RunServiceChecks", 1, RunServiceChecks)
 
-PlayerRow:Fill()
 Window:ShowTab(TabMain)
 Window:Center()
