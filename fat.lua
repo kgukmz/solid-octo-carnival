b78332cc92a40207b03af77e54f1fe0a6b307ce9
el-the-mundo <130784209+el-the-mundo@users.noreply.github.com>
2025-01-13 22:28:24 +0000

    Jamada

---
+++ fat.lua
@@ -0,0 +1,12 @@
+
+local ImGui = loadstring(game:HttpGet('https://github.com/depthso/Roblox-ImGUI/raw/main/ImGui.lua'))()
+
+local Window = ImGui:CreateWindow({
+	Title = "Alchemy" .. " | " .. IdentifyExecutor,
+	Size = UDim2.fromOffset(400, 250),
+	Position = UDim2.new(0.5, 0, 0, 70),
+    NoResize = true,
+})
+
+
+Window:Center()
--- loader.lua
+++ loader.lua
@@ -1,16 +1,12 @@
-local ParentDirectory = "https://raw.githubusercontent.com/kgukmz/solid-octo-carnival/main/"
+local OldRequire = require
 
-local ScriptStartTick = tick()
+local function DirectoryRequire(Path)
+	if (type(Path) ~= "string") then
+		return OldRequire(Path)
+	end
 
+	local ParentDirectory = "https://raw.githubusercontent.com/kgukmz/solid-octo-carnival/main/"
+	local JapanU20Match = ParentDirectory 
+end
 
-local ImGui = loadstring(game:HttpGet('https://github.com/depthso/Roblox-ImGUI/raw/main/ImGui.lua'))()
-
-local Window = ImGui:CreateWindow({
-	Title = "Alchemy" .. " | " .. IdentifyExecutor,
-	Size = UDim2.fromOffset(400, 250),
-	Position = UDim2.new(0.5, 0, 0, 70),
-    NoResize = true,
-})
-
-
-Window:Center()
+getgenv().require = DirectoryRequire
