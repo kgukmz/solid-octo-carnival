42043ce70e93f0f6a371b5b9b099959d84701428
el-the-mundo <130784209+el-the-mundo@users.noreply.github.com>
2025-01-13 23:14:38 +0000

    AHHH

---
+++ Setup.lua
@@ -0,0 +1,25 @@
+local OldRequire = require
+
+local function DirectoryRequire(Path)
+	if (type(Path) ~= "string") then
+		return OldRequire(Path)
+	end
+
+	local ParentDirectory = "https://raw.githubusercontent.com/kgukmz/solid-octo-carnival/main/"
+	local JapanU20Match = ParentDirectory .. Path
+
+	local Success, Response = pcall(function()
+		return game:HttpGet(JapanU20Match)
+	end)
+
+	if (string.match(Response, "404")) then
+		warn("404 Error")
+		return nil
+	end
+
+	return loadstring(Response)()
+end
+
+getgenv().require = DirectoryRequire
+
+DirectoryRequire("Loader.lua")
