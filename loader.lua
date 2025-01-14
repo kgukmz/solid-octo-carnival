cd29bbad054ee0c2efd74f775a6be767b5f954d7
el-the-mundo <130784209+el-the-mundo@users.noreply.github.com>
2025-01-14 22:24:04 +0000

    yah

--- loader.lua
+++ loader.lua
@@ -1,19 +1,23 @@
-if (getgenv().AlchemyLoaded == true) then
-	return
-end
-
-print("v")
-
-getgenv().Library = require("modules/ImGui.lua")
-
-local Services = require("modules/GetServices.lua")
-local GamesList = require("SupportedGames.json")
-
-local HttpService = Services:Get("HttpService")
-local SupportedGames = HttpService:JSONEncode(GamesList)
-
-local GameScript = require(string.format("games/%s/main.lua", SupportedGames[game.PlaceId]))
-
-if (not GameScript) then
-	warn("no game script")
-end
+xpcall(function()
+	if (getgenv().AlchemyLoaded == true) then
+		return
+	end
+	
+	print("v")
+	
+	getgenv().Library = require("modules/ImGui.lua")
+	
+	local Services = require("modules/GetServices.lua")
+	local GamesList = require("SupportedGames.json")
+	
+	local HttpService = Services:Get("HttpService")
+	local SupportedGames = HttpService:JSONEncode(GamesList)
+	
+	local GameScript = require(string.format("games/%s/main.lua", SupportedGames[game.PlaceId]))
+	
+	if (not GameScript) then
+		warn("no game script")
+	end
+end, function(...)
+	table.foreach({...}, warn)
+end)
