c6ce5bbfd6d34265835dd15d732d1816ad22f8ed
el-the-mundo <130784209+el-the-mundo@users.noreply.github.com>
2025-01-14 17:37:55 +0000

    one shot match

--- loader.lua
+++ loader.lua
@@ -1,1 +1,10 @@
-print("This is where all the real stuff begins!")
+local ImGui = require("modules/ImGui.lua")
+local Window = ImGui:CreateWindow({
+	Title = "Alchemy" .. " | " .. identifyexecutor(),
+	Size = UDim2.fromOffset(400, 250),
+	Position = UDim2.new(0.5, 0, 0, 70),
+    NoResize = true,
+})
+
+
+Window:Center()
